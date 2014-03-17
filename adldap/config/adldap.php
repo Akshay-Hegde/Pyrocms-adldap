b6197614de14f9d4e15191dda15d798138bd2e8b
lanuser <lanuser@lanuser.ru>
2014-03-17 15:09:53 +0400

    fre

--- adldap/config/adldap.php
+++ adldap/config/adldap.php
@@ -1,10 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['account_suffix']	= '@megafon-retail.ru';
-$config['base_dn']		= 'DC=megafon-retail,DC=ru';
-$config['domain_controllers']	= array ('10.183.248.2');
-$config['ad_username']		= 'e.bronnikov';
-$config['ad_password']		= 'Az160259';
+$config['account_suffix']	= '';
+$config['base_dn']		= '';
+$config['domain_controllers']	= array ('');
+$config['ad_username']		= '';
+$config['ad_password']		= '';
 $config['real_primarygroup']	= true;
 $config['use_ssl']		= false;
 $config['use_tls'] 		= false;
